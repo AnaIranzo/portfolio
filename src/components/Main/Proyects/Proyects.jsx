aebabfec106854a86ef8d9a3b8b58a60f191eb7f
AnaIranzo <aeiranzom@gmail.com>
2023-10-25 12:54:22 +0200

    add proyects

--- src/components/Main/Proyects/Proyects.jsx
+++ src/components/Main/Proyects/Proyects.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 class Proyects extends Component {
   render() {
-    return <section>
-      <article></article>
-      <article></article>
-      <article></article>
-      <article></article>
-      <article></article>
+    return <section className="proyects">
+      <h1>Proyects</h1>
+      <article><img src="" alt="" srcset="" /></article>
+      <article><img src="" alt="" srcset="" /></article>
+      <article><img src="" alt="" srcset="" /></article>
+      <article><img src="" alt="" srcset="" /></article>
+      <article><img src="" alt="" srcset="" /></article>
     </section>
   }
 }
